5e5bf4e1f9550954b132db3b70fd51b174505ecd
AdamBucko1 <xbuckoa@stuba.sk>
2023-02-15 16:05:46 +0100

    thrid commit

--- main.py
+++ main.py
@@ -15,9 +15,16 @@ cam.open_device()
 cam.set_exposure(10000)
 cam.set_param('imgdataformat','XI_RGB32')
 cam.set_param('auto_wb', 1)
-
-
-
+img = xiapi.Image()
 cam.start_acquisition()
 
 while cv2.waitKey() != ord('q'):
+    cam.get_image(img)
+    image = img.get_image_data_numpy()
+    image= cv2.resize(image,(240,240))
+    cv2.imshow("test",image)
+    cv2.waitKey()
+
+
+cam.stop_acquisition()
+cam.close_device()
