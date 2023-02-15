b5a5c450f935214bbd2223c05d0021630e0ffcc0
Daniel-Fundarek <xfundarekd@stuba.sk>
2023-02-15 15:58:02 +0100

    second commit

--- main.py
+++ main.py
@@ -1,5 +1,23 @@
+from ximea import xiapi
 import cv2
 
-print("Hello")
-img = None
-cv2.imshow(img)
+
+# create instance for first connected camera
+cam = xiapi.Camera()
+
+
+
+print("Open Camera")
+
+
+cam.open_device()
+
+cam.set_exposure(10000)
+cam.set_param('imgdataformat','XI_RGB32')
+cam.set_param('auto_wb', 1)
+
+
+
+cam.start_acquisition()
+
+while cv2.waitKey() != ord('q'):
