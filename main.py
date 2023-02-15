2671034ea62e339787d46a92d4bdb27c2f91e284
AdamBucko1 <xbuckoa@stuba.sk>
2023-02-15 16:28:20 +0100

    custom camera

--- main.py
+++ main.py
@@ -3,24 +3,26 @@ import cv2
 
 
 # create instance for first connected camera
-cam = xiapi.Camera()
+#cam = xiapi.Camera()
+cam=cv2.VideoCapture(0)
 
 
-
-print("Open Camera")
-
-
-cam.open_device()
-
-cam.set_exposure(10000)
-cam.set_param('imgdataformat','XI_RGB32')
-cam.set_param('auto_wb', 1)
-img = xiapi.Image()
-cam.start_acquisition()
+# print("Open Camera")
+#
+#
+# cam.open_device()
+#
+# cam.set_exposure(10000)
+# cam.set_param('imgdataformat','XI_RGB32')
+# cam.set_param('auto_wb', 1)
+# img = xiapi.Image()
+# cam.start_acquisition()
 
 for i in range(4):
-    cam.get_image(img)
-    image = img.get_image_data_numpy()
+    ret,image=cam.read()
+    #cam.get_image(img)
+
+    #image = img.get_image_data_numpy()
     image = cv2.resize(image,(240,240))
     cv2.imshow("test",image)
     cv2.waitKey()
