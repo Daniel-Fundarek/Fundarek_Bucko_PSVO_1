33aaa1484e0b0c907840767e449b60e5ec007783
AdamBucko1 <xbuckoa@stuba.sk>
2023-02-19 18:57:38 +0100

    Additional refactor+ red color fnc

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 # from ximea import xiapi
 import cv2
 import numpy as np
-
+from enum import Enum
 # create instance for first connected camera
 # cam = xiapi.Camera()
+class ColorChannel(Enum):
+    RED = 0
+    GREEN = 1
+    BLUE = 2
+
 cam = cv2.VideoCapture(0)
 
 
@@ -21,8 +26,8 @@ def rotate_image(image):
     height, width, channels = image.shape
     array = np.empty(shape=(width, height, channels), dtype='uint8')
     for i in range(height):
-        item = image[-i]
-        array[:, i] = item
+        pixel_row = image[-i]
+        array[:, i] = pixel_row #rotate pixel_row to pixel column
     return array
 
 
@@ -48,64 +53,56 @@ def capture_webcam_images(count):
     cv2.destroyAllWindows()
     return images
 
-def select_red_channel(jechu:int):
-    """
+def select_red_channel(image,channel:ColorChannel):
+    b, g, r = cv2.split(image)
 
-  <<<<<<< 4-task-4-create-method-which-rotate-image-use-for-loop
-  save_webcam_images(4)
-  image = cv2.imread('resources/img1.png')
-  img_rotated = rotate_image(image)
-  cv2.imshow("tdest", img_rotated)
-  cv2.waitKey(0)
+    image_merge = cv2.merge([r, g, b])
+
+
+def main():
+  img = capture_webcam_images(4)
+  #array = [tuple(img[1:3]), tuple(img[0:2]), (img[2], img[1])] #num of touples == columns  #num of items in touples== rows
+  img_stack=[(img[0],img[2]),(img[1],img[3])]
+  mosaique = create_mosaique(img_stack)
+  cv2.imshow('mosaique', mosaique)
+  cv2.imwrite("resources/mosaique.png",mosaique)
+  rotated_image=rotate_image(img[2])
+  cv2.imshow('img2', rotated_image)
+  cv2.waitKey()
   cv2.destroyAllWindows()
-  =======
-      :param jechu:
-      :return:
-      """
-      pass
-  def main():
-      img = capture_webcam_images(4)
-      #array = [tuple(img[1:3]), tuple(img[0:2]), (img[2], img[1])] #num of touples == columns  #num of items in touples== rows
-      img_stack=[(img[0],img[2]),(img[1],img[3])]
-      mosaique = create_mosaique(img_stack)
-      cv2.imshow('mosaique', mosaique)
-      cv2.imwrite("resources/mosaique.png",mosaique)
-      cv2.waitKey()
-      cv2.destroyAllWindows()
 
 
-  def create_mosaique(image_collection: list):
-      """
+def create_mosaique(image_collection: list):
+  """
 
-      :param image_collection: List[tuple[horizontal pictures], tuple[horizontal pictures],...]
-      you can add as many tuples to list as you want. Each tuple creates new vertical layer.
-      Items in tuple are horizontal pictures in mosaique. you can add as many items to tuple.
-      len(List) = num of vertical layers
-      len(tuple) = horizontal items number
+  :param image_collection: List[tuple[horizontal pictures], tuple[horizontal pictures],...]
+  you can add as many tuples to list as you want. Each tuple creates new vertical layer.
+  Items in tuple are horizontal pictures in mosaique. you can add as many items to tuple.
+  len(List) = num of vertical layers
+  len(tuple) = horizontal items number
 
-      :return: ndarray - containing concatenated all images into one according to input param
-      """
-      h_image_count = len(image_collection[0])
-      height, width, channels = image_collection[0][0].shape
-      output_array = np.zeros((h_image_count * height, 0, channels), dtype='uint8')
+  :return: ndarray - containing concatenated all images into one according to input param
+  """
+  h_image_count = len(image_collection[0])
+  height, width, channels = image_collection[0][0].shape
+  output_array = np.zeros((h_image_count * height, 0, channels), dtype='uint8')
 
-      for horizontal in image_collection:
-          output_array = np.concatenate((output_array, np.concatenate(horizontal, axis=0)), axis=1)
-      return output_array
+  for horizontal in image_collection:
+      output_array = np.concatenate((output_array, np.concatenate(horizontal, axis=0)), axis=1)
+  return output_array
 
 
-  if __name__ == "__main__":
-      main()
-  >>>>>>> main
+if __name__ == "__main__":
+  main()
 
 '''
 while cv2.waitKey() != ord('q'):
-    cam.get_image(img)
-    image = img.get_image_data_numpy()
-    image = cv2.resize(image,(240,240))
-    cv2.imshow("test",image)
-    cv2.waitKey()
-    cv2.imwrite(img)
+cam.get_image(img)
+image = img.get_image_data_numpy()
+image = cv2.resize(image,(240,240))
+cv2.imshow("test",image)
+cv2.waitKey()
+cv2.imwrite(img)
 
 '''
 
