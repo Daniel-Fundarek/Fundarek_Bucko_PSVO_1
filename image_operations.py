91a8fb8388d9f8697ec58e8f8b7c39332df7ad08
AdamBucko1 <xbuckoa@stuba.sk>
2023-02-22 16:34:42 +0100

    Program made camera-universal(swapable between ntb camera and ximea camera)

--- image_operations.py
+++ image_operations.py
@@ -1,70 +1,105 @@
 import cv2
 import numpy as np
+from ximea import xiapi
+
 
 def rotate_image(image):
     height, width, channels = image.shape
     array = np.empty(shape=(width, height, channels), dtype='uint8')
     for i in range(height):
         pixel_row = image[-i]
-        array[:, i] = pixel_row #rotate pixel_row to pixel column
+        array[:, i] = pixel_row  # rotate pixel_row to pixel column
     return array
 
 
-def capture_webcam_images(count,cam):
+def capture_webcam_images(count, camera='ntb'):  # cam
+    cam = None
     images = []
-    for i in range(count):
-        ret, image = cam.read()
-        # cam.get_image(img)
 
-        # image = img.get_image_data_numpy()
-        image = cv2.resize(image, (240, 240))
-        images.append(image)
-        cv2.imshow("test", image)
+    if camera == "ximea":
+        cam = xiapi.Camera()
+        print("Open Camera")
+        cam.open_device()
+        cam.set_exposure(10000)
+        cam.set_param('imgdataformat', 'XI_RGB32')
+        cam.set_param('auto_wb', 1)
+        img = xiapi.Image()
+        cam.start_acquisition()
 
+        for i in range(count):
+            cam.get_image(img)
 
+            image = img.get_image_data_numpy()
+            image = cv2.cvtColor(image, cv2.COLOR_RGBA2BGR)
 
-        filepath = f'resources/img{i}.png'
-        key = 0
-        while key != 32:
-            key = cv2.waitKey()
-        cv2.imwrite(filepath, image)
-        print(f' Image {filepath} is saved')
+            image = cv2.resize(image, (240, 240))
+            images.append(image)
+            cv2.imshow("test", image)
+
+            filepath = f'resources/img{i}.png'
+            key = 0
+            while key != 32:
+                key = cv2.waitKey()
+            cv2.imwrite(filepath, image)
+            print(f' Image {filepath} is saved')
+
+        cam.stop_acquisition()
+        cam.close_device()
+
+    if camera == 'ntb':
+        cam = cv2.VideoCapture(0)
+
+        for i in range(count):
+            ret, image = cam.read()
+
+            image = cv2.resize(image, (240, 240))
+            images.append(image)
+            cv2.imshow("test", image)
+
+            filepath = f'resources/img{i}.png'
+            key = 0
+            while key != 32:
+                key = cv2.waitKey()
+            cv2.imwrite(filepath, image)
+            print(f' Image {filepath} is saved')
+
     cv2.destroyAllWindows()
+
     return images
 
-def select_red_channel(image,color:str):
+
+def select_red_channel(image, color: str):
     b, g, r = cv2.split(image)
-   # if color!="blue"or"green"or"red":
+    # if color!="blue"or"green"or"red":
     #    return image
-    if color=="blue":
-        colored_image = cv2.merge([b, g * 0, r*0])
+    if color == "blue":
+        colored_image = cv2.merge([b, g * 0, r * 0])
 
     if color == "green":
-        colored_image = cv2.merge([b * 0, g, r*0])
+        colored_image = cv2.merge([b * 0, g, r * 0])
 
     if color == "red":
-        colored_image = cv2.merge([b*0, g*0, r])
+        colored_image = cv2.merge([b * 0, g * 0, r])
 
     return colored_image
 
-def apply_kernel_filter(mosaique,width_start,width_stop,height_start,height_stop):
+
+def apply_kernel_filter(mosaique, width_start, width_stop, height_start, height_stop):
     kernel = np.array([[0, -1, 0],
                        [-1, 5, -1],
                        [0, -1, 0]], np.float32)  # kernel should be floating point type
-    #filtered_mosaique=mosaique
-    #for i in range(width_stop):
-    #    for j in range(height_stop):
-    #        filtered_mosaique[i,j,:] = cv2.filter2D(mosaique[i,j,:], -1, kernel)
-    #filtered_mosaique=mosaique[width_start:width_stop,height_start:height_stop,:]
-    mosaique[width_start:width_stop,height_start:height_stop,:]=cv2.filter2D(mosaique[width_start:width_stop,height_start:height_stop,:], -1, kernel)
-    #filtered_image = cv2.filter2D(mosaique, -1, kernel)
-    # ddepth = -1, means destination image has depth same as input image
+
+    mosaique[width_start:width_stop, height_start:height_stop, :] = \
+        cv2.filter2D(mosaique[width_start:width_stop, height_start:height_stop, :], -1, kernel)
     return mosaique
+
+
 def print_img_info(image):
     print(
         f"Width is: {len(image[0, :, :])} "
         f"height is: {len(image[:, 0, :])} "
         f"and the data type is:{image.dtype}")
+
 
 def create_mosaique(image_collection: list):
     """
--- main.py
+++ main.py
@@ -1,52 +1,47 @@
-# from ximea import xiapi
+
 import cv2
 import image_operations as img_fcn
 import numpy as np
 from enum import Enum
+
 # create instance for first connected camera
-# cam = xiapi.Camera()
-
-cam = cv2.VideoCapture(0)
 
 
-# print("Open Camera")
-#
-#
-# cam.open_device()
-#
-# cam.set_exposure(10000)
-# cam.set_param('imgdataformat','XI_RGB32')
-# cam.set_param('auto_wb', 1)
-# img = xiapi.Image()
-# cam.start_acquisition()
+#cam = cv2.VideoCapture(0)
+
+
 
 def main():
-  img = img_fcn.capture_webcam_images(4,cam)
-  #array = [tuple(img[1:3]), tuple(img[0:2]), (img[2], img[1])] #num of touples == columns  #num of items in touples== rows
-  img_stack=[(img[0],img[2]),(img[1],img[3])]
-  mosaique = img_fcn.create_mosaique(img_stack)
-  cv2.imshow('mosaique', mosaique)
-  cv2.imwrite("resources/mosaique.png", mosaique)
-  filtered_mosaique=img_fcn.apply_kernel_filter(mosaique[0:240,0:240,:],100,200,100,200)
-  cv2.imshow('kernel_filtered_mosaique', filtered_mosaique)
-  cv2.imwrite("resources/kernel_filtered_mosaique.png",filtered_mosaique)
-  rotated_image=img_fcn.rotate_image(img[2])
-  cv2.imshow('rotated_image', rotated_image)
-  cv2.imwrite("resources/rotated_image.png", rotated_image)
-  red_image=img_fcn.select_red_channel(img[1],"red")
-  cv2.imshow('red_image', red_image)
-  cv2.imwrite("resources/red_image.png", red_image)
-  img_fcn.print_img_info(red_image)
-  cv2.waitKey()
+    picture_size = 240
+    #img = img_fcn.capture_webcam_images(4,cam)
+    img = img_fcn.capture_webcam_images(4,"ximea")
+    # array = [tuple(img[1:3]), tuple(img[0:2]), (img[2], img[1])] #num of touples == columns  #num of items in touples== rows
+    img_stack = [(img[0], img[2]), (img[1], img[3])]
+    mosaique = img_fcn.create_mosaique(img_stack)
 
-  cv2.destroyAllWindows()
+    cv2.imshow('mosaique', mosaique)
+    cv2.imwrite("resources/mosaique.png", mosaique)
 
+    mosaique[0:picture_size, 0:picture_size, :] = img_fcn.apply_kernel_filter(
+        mosaique[0:picture_size, 0:picture_size, :], 100, 200, 100, 200)
 
+    mosaique[picture_size:picture_size * 2, 0:picture_size, :] = img_fcn.rotate_image(
+        mosaique[picture_size:picture_size * 2, 0:picture_size, :])
 
+    mosaique[0:picture_size, picture_size:picture_size * 2, :] = img_fcn.select_red_channel(
+        mosaique[0:picture_size, picture_size:picture_size * 2, :], "red")
+
+    cv2.imshow('mosaique_post_processing', mosaique)
+    cv2.imwrite("resources/mosaique.png", mosaique)
+
+    img_fcn.print_img_info(mosaique)
+    cv2.waitKey()
+
+    cv2.destroyAllWindows()
 
 
 if __name__ == "__main__":
-  main()
+    main()
 
 '''
 while cv2.waitKey() != ord('q'):
