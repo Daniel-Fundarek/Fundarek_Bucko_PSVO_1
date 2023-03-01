ae5722db838d9c81309e64df4b036aa9282d2289
AdamBucko1 <xbuckoa@stuba.sk>
2023-03-01 15:25:24 +0100

    Structure Refactor

--- Zadanie_2/main.py
+++ Zadanie_2/main.py
@@ -4,18 +4,9 @@ import image_operations as img_fcn
 import numpy as np
 from enum import Enum
 
-# create instance for first connected camera
-
-
-#cam = cv2.VideoCapture(0)
-
-
-
 def main():
     picture_size = 240
-    #img = img_fcn.capture_webcam_images(4,cam)
-    img = img_fcn.capture_webcam_images(4,"ximea")
-    # array = [tuple(img[1:3]), tuple(img[0:2]), (img[2], img[1])] #num of touples == columns  #num of items in touples== rows
+    img = img_fcn.capture_webcam_images(4,"ximea")#ntb or xiemea
     img_stack = [(img[0], img[2]), (img[1], img[3])]
     mosaique = img_fcn.create_mosaique(img_stack)
 
@@ -28,7 +19,7 @@ def main():
     mosaique[picture_size:picture_size * 2, 0:picture_size, :] = img_fcn.rotate_image(
         mosaique[picture_size:picture_size * 2, 0:picture_size, :])
 
-    mosaique[0:picture_size, picture_size:picture_size * 2, :] = img_fcn.select_red_channel(
+    mosaique[0:picture_size, picture_size:picture_size * 2, :] = img_fcn.select_color_channel(
         mosaique[0:picture_size, picture_size:picture_size * 2, :], "red")
 
     cv2.imshow('mosaique_post_processing', mosaique)
@@ -42,17 +33,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
-'''
-while cv2.waitKey() != ord('q'):
-cam.get_image(img)
-image = img.get_image_data_numpy()
-image = cv2.resize(image,(240,240))
-cv2.imshow("test",image)
-cv2.waitKey()
-cv2.imwrite(img)
-
-'''
-
-# cam.stop_acquisition()
-# cam.close_device()
