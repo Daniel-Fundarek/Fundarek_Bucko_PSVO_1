52de13ad5b3017ce137230f96483a139dd036395
Daniel-Fundarek <xfundarekd@stuba.sk>
2023-03-07 17:30:23 +0100

    merge fix

--- Zadanie_2/main.py
+++ Zadanie_2/main.py
@@ -6,7 +6,7 @@ from enum import Enum
 def main2():
     picture_size = 240
     img_fcn.camera_calibration(6, 8)
-    img = img_fcn.capture_webcam_images("ximea")#ntb or xiemea
+    img = img_fcn.capture_webcam_images("ximea",picture_size)#ntb or xiemea
     cv2.waitKey()
     cv2.destroyAllWindows()
 
