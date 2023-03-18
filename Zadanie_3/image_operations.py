b284fb8a13f385ba88d94f0bf248127f572b8060
AdamBucko1 <xbuckoa@stuba.sk>
2023-03-18 14:34:58 +0100

    Update

--- Zadanie_3/image_operations.py
+++ Zadanie_3/image_operations.py
@@ -2,206 +2,103 @@ import cv2
 import numpy as np
 import glob
 from ximea import xiapi
+import numpy as np
+from skimage import io, color
+import skimage as ski
+def hough_transform_circle_second(img, radius_range, min_votes):
+    rows, cols = img.shape
+    rmin, rmax = radius_range
+    rrange = rmax - rmin
 
-mtx=None
-dist=None
-newcameramtx=None
-roi=None
-h=None
-w=None
-rvecs=None
-tvecs=None
-def rotate_image(image):
-    height, width, channels = image.shape
-    array = np.empty(shape=(width, height, channels), dtype='uint8')
-    for i in range(height):
-        pixel_row = image[-i]
-        array[:, i] = pixel_row  # rotate pixel_row to pixel column
-    return array
-def openNPZ():
-    data = np.load('camera_calibration.npz')
-    lst = data.files
-    for item in lst:
-        print(item)
-        print(data[item])
+    # Initialize accumulator array
+    accumulator = np.zeros((rows, cols, rrange))
 
-def capture_webcam_images(img_size, camera='ntb'):  # cam
+    # Precompute sin and cos values
+    thetas = np.linspace(0, 2 * np.pi, 100)
+    cos_thetas = np.cos(thetas)
+    sin_thetas = np.sin(thetas)
 
-    images = []
-    i = 0
-    filepath = f'resources/chessboard'
-    key = 0
-    if camera == "ximea":
-        cam = xiapi.Camera()
-        print("Open Camera")
-        cam.open_device()
-        cam.set_exposure(10000)
-        cam.set_param('imgdataformat', 'XI_RGB32')
-        cam.set_param('auto_wb', 1)
-        img = xiapi.Image()
-        cam.start_acquisition()
+    # Loop over each radius to search for
+    for r_idx, r in enumerate(range(rmin, rmax)):
+        print(f'Processing radius: {r}')
 
-        while key != ord('q'):
+        # Create circle template with radius r
+        circle = np.zeros((2 * r + 1, 2 * r + 1))
+        y, x = np.ogrid[-r:r+1, -r:r+1]
+        mask = x**2 + y**2 <= r**2
+        circle[mask] = 1
 
-            cam.get_image(img)
-            image = img.get_image_data_numpy()
-            # image = cv2.cvtColor(image, cv2.COLOR_RGBA2BGR)
-            image = cv2.cvtColor(image, cv2.COLOR_BGRA2BGR)
-            ########### aplikacia  kalibracnej matice na fotku
-            undistorted_image = cv2.undistort(image, mtx, dist, None, newcameramtx)
-            x, y, w, h = roi
-            image = undistorted_image[y:y + h, x:x + w]
-            image = detect_circle(image)
-            image = cv2.resize(image, img_size)
+        # Convolve image with circle template to count edge pixels
+        convolved = cv2.filter2D(img, -1, circle)
 
-            cv2.imshow("preview", image)
-            if key == ord(' '):
-                images.append(image)
-                key = 0
-                filepath1 = filepath + str(i) + '.png'
-                cv2.imwrite(filepath1, image)
-                print(f' Image {filepath1} is saved')
-                i += 1
+        # Loop over each edge pixel in the image
+        edge_pixels = np.argwhere(convolved > 0)
+        for y, x in edge_pixels:
 
-            key = cv2.waitKey(100)
+            # Calculate the possible circle center coordinates for this pixel and radius
+            xs = x - r * sin_thetas
+            ys = y + r * cos_thetas
+            # Find the valid indices where xs_int and ys_int are within the image dimensions
+            valid_indices = np.where((xs >= 0) & (xs < cols-1) & (ys >= 0) & (ys < rows-1))
 
-        cam.stop_acquisition()
-        cam.close_device()
+            # Increment the accumulator array at the valid center coordinates
+            xs_int, ys_int = np.round(xs[valid_indices]).astype(int), np.round(ys[valid_indices]).astype(int)
+            accumulator[ys_int, xs_int, r_idx] += 1
 
-    if camera == 'ntb':
-        cam = cv2.VideoCapture(0)
+    # Find circles with the highest number of votes
+    max_votes = np.max(accumulator, axis=2)
+    max_coords = np.argwhere(max_votes > min_votes)
+    max_radii = rmin + np.argmax(accumulator, axis=2)[max_coords[:, 0], max_coords[:, 1]]
+    circles = np.concatenate((max_coords, np.reshape(max_radii, (-1, 1))), axis=1)
 
-        while key != ord('q'):
+    return circles
 
-            ret, image = cam.read()
-            ########### aplikacia  kalibracnej matice na fotku
-            undistorted_image = cv2.undistort(image, mtx, dist, None, newcameramtx)
-            x, y, w, h = roi
-            image = undistorted_image[y:y + h, x:x + w]
-            image = cv2.resize(image, img_size)
-            image = detect_circle(image)
-            cv2.imshow("preview", image)
-            if key == ord(' '):
-                images.append(image)
-                key = 0
-                filepath1 = filepath + str(i) + '.png'
-
-                cv2.imwrite(filepath1, image)
-                print(f' Image {filepath1} is saved')
-                i += 1
-
-            key = cv2.waitKey(100)
-
-    cv2.destroyAllWindows()
-    return images
-
-
-def detect_circle(img = None):
-    # img = cv2.imread("circles/circle0.png")
-    gauss = cv2.GaussianBlur(img, (7, 7), 1.5)
-    gray = cv2.cvtColor(gauss, cv2.COLOR_BGR2GRAY)
-
-
-    print("image loaded")
-
-    circles = cv2.HoughCircles(gray, cv2.HOUGH_GRADIENT_ALT, 2, 1000, param1=300, param2=0.5, minRadius=50, maxRadius=0)
-    # circles = cv2.HoughCircles(gray, cv2.HOUGH_GRADIENT, 3, 1800, param1=220, param2=95, minRadius=100, maxRadius=600)
-    print("sds")
-    # cv2.imshow("circle", ~gray)
-    if circles is not None:
-        circles = np.uint16(np.around(circles))
-        for i in circles[0, :]:
-            # draw the outer circle
-            cv2.circle(img, (i[0], i[1]), i[2], (0, 255, 0), 2)
-            # draw the center of the circle
-            cv2.circle(img, (i[0], i[1]), 2, (0, 0, 255), 3)
-    # cv2.imshow("circle",img)
-    return img
-
-
-def camera_calibration(vert_squares, horiz_squares):
-    global mtx
-    global dist
-    global newcameramtx
-    global roi
-    global h
-    global w
-    global rvecs
-    global tvecs
-    # Defining the dimensions of checkerboard
-    CHECKERBOARD = (vert_squares, horiz_squares)
-    criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
-
-    # Creating vector to store vectors of 3D points for each checkerboard image
-    objpoints = []
-    # Creating vector to store vectors of 2D points for each checkerboard image
-    imgpoints = []
-
-    # Defining the world coordinates for 3D points
-    obj_array = np.zeros((1, CHECKERBOARD[0] * CHECKERBOARD[1], 3), np.float32)
-    obj_array[0, :, :2] = np.mgrid[0:CHECKERBOARD[0], 0:CHECKERBOARD[1]].T.reshape(-1, 2)
-    prev_img_shape = None
-
-    # Extracting path of individual image stored in a given directory
-    images = glob.glob('./resources/*.png')
-    for fname in images:
-        img = cv2.imread(fname)
-        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-        # Find the chess board corners
-        # If desired number of corners are found in the image then ret = true
-        #ret, corners = cv2.findChessboardCorners(gray, CHECKERBOARD,
-        #                                         cv2.CALIB_CB_ADAPTIVE_THRESH + cv2.CALIB_CB_FAST_CHECK + cv2.CALIB_CB_NORMALIZE_IMAGE)
-        ret, corners = cv2.findChessboardCorners(gray, CHECKERBOARD, None)
-
-        """
-        If desired number of corner are detected,
-        we refine the pixel coordinates and display 
-        them on the images of checker board
-        """
-        if ret == True:
-            print("found")
-            objpoints.append(obj_array)
-            # refining pixel coordinates for given 2d points.
-            corners2 = cv2.cornerSubPix(gray, corners, (11, 11), (-1, -1), criteria)
-
-            imgpoints.append(corners2)
-
-            # Draw and display the corners
-            img = cv2.drawChessboardCorners(img, CHECKERBOARD, corners2, ret)
-
-        cv2.imshow('img', img)
-        cv2.waitKey(0)
-
-    cv2.destroyAllWindows()
-
-    h, w = img.shape[:2]
-
+def hough_transform_circle(img, radius_min, radius_max, threshold):
     """
-    Performing camera calibration by 
-    passing the value of known 3D points (objpoints)
-    and corresponding pixel coordinates of the 
-    detected corners (imgpoints)
+    Detect circles in an image using the Hough Transform.
+
+    Parameters:
+    img (ndarray): The input image as a grayscale or color image.
+    radius_min (int): The minimum radius to search for.
+    radius_max (int): The maximum radius to search for.
+    threshold (int): The minimum number of votes required for a circle to be detected.
+
+    Returns:
+    A list of tuples (x, y, r) representing the detected circles, where (x, y) is the center and r is the radius.
     """
-    ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, gray.shape[::-1], None, None)
-    if ret:
-        np.savez('camera_calibration.npz', fx=mtx[0, 0], fy=mtx[1, 1], cx=mtx[0, 2], cy=mtx[1, 2])
-        print("Camera matrix : \n")
-        print(mtx)
-        print("dist : \n")
-        print(dist)
-        print("rvecs : \n")
-        print(rvecs)
-        print("tvecs : \n")
-        print(tvecs)
-    h, w = img.shape[:2]
-    newcameramtx, roi = cv2.getOptimalNewCameraMatrix(mtx, dist, (w, h), 1, (w, h))
-def undistor_images():
-    images = glob.glob('./resources/*.png')
-    for fname in images:
-        distorted_image = cv2.imread(fname)
-        cv2.imshow('distorted_image', distorted_image)
-        undistorted_image = cv2.undistort(distorted_image, mtx, dist, None, newcameramtx)
-        x, y, w, h = roi
-        undistorted_image = undistorted_image[y:y + h, x:x + w]
-        cv2.imshow('undst', undistorted_image)
-        cv2.waitKey(0)
+
+    # Convert the input image to grayscale
+    if len(img.shape) > 2:
+        img = ski.rgb2gray(img)
+
+    # Create an accumulator array with dimensions (num_rows, num_cols, num_radii)
+    rows, cols = img.shape
+    radii = np.arange(radius_min, radius_max+1)
+    accumulator = np.zeros((rows, cols, radii.size), dtype=np.uint16)
+
+    # Define the range of theta values (in radians) to search for
+    thetas = np.deg2rad(np.arange(0, 360))
+
+    # Loop over each edge pixel in the image
+    edge_pixels = np.argwhere(img > 0)
+    for y, x in edge_pixels:
+
+        # Loop over each radius to search for
+        for r_idx, r in enumerate(radii):
+
+            # Calculate the possible circle center coordinates for this pixel and radius
+            xs = x - r * np.sin(thetas)
+            ys = y + r * np.cos(thetas)
+            # Find the valid indices where xs_int and ys_int are within the image dimensions
+            valid_indices = np.where((xs >= 0) & (xs < cols-1) & (ys >= 0) & (ys < rows-1))
+
+            # Increment the accumulator array at the valid center coordinates
+            xs_int, ys_int = np.round(xs[valid_indices]).astype(int), np.round(ys[valid_indices]).astype(int)
+            accumulator[ys_int, xs_int, r_idx] += 1
+            # Increment the accumulator array at the possible center coordinates
+        print(f"row: {y} column: {x}")
+    # Find the (x, y, r) coordinates of the circle centers with the most votes
+    candidates = np.argwhere(accumulator >= threshold)
+    circle_coords = [(x, y, radii[r_idx]) for y, x, r_idx in candidates]
+
+    return circle_coords
--- Zadanie_3/main.py
+++ Zadanie_3/main.py
@@ -1,8 +1,11 @@
 import cv2
-from Zadanie_2 import image_operations as img_fcn
+from Zadanie_3 import image_operations as img_fcn
 import numpy as np
 from enum import Enum
-
+from skimage import io, color
+import skimage as ski
+import matplotlib.pyplot as plt
+from skimage.draw import circle_perimeter
 def main2():
     picture_size = (1920,1200)
     #img_fcn.camera_calibration(6, 8)
@@ -10,7 +13,39 @@ def main2():
     #img_fcn.undistor_images()
     #img = img_fcn.capture_webcam_images(picture_size,"ntb")#ntb or xiemea
     #
-    img_fcn.openNPZ()
+    # Load the image
+    image = io.imread('resources/blue_circle_bigger.png')
+
+    image = image[:, :, :3]  # remove alpha channel
+    # Convert the image to grayscale
+    gray_image = color.rgb2gray(image)
+
+    # Apply Hough Transform to detect the circles
+    circles = img_fcn.hough_transform_circle_second(gray_image, (20, 50), 100)
+
+    # Draw the detected circles on the original image
+    for circle in circles:
+        rr, cc = circle_perimeter(circle[0], circle[1], circle[2])
+
+        # Create masks for the coordinates that are in bounds
+        y_mask = np.logical_and(rr >= 0, rr < image.shape[0])
+        x_mask = np.logical_and(cc >= 0, cc < image.shape[1])
+        mask = np.logical_and(x_mask, y_mask)
+
+        # Apply the masks to the coordinate arrays
+        rr = rr[mask]
+        cc = cc[mask]
+
+        # Set the circle pixels to red
+        image[rr, cc] = [0, 255, 0]
+
+    # Display the image with the detected circles
+   # plt.imshow(image)
+   # plt.show()
+    image_orig = io.imread('resources/blue_circle.png')
+    cv2.imshow("preview", image_orig)
+    cv2.imshow("detected", image)
+    #img_fcn.openNPZ()
     # img_fcn.detect_circle()
     cv2.waitKey()
     cv2.destroyAllWindows()
